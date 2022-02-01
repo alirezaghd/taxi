578dedde541d227f8ae8ed4f0f4e2bb7b260222f
alirezagholamrezaei <alireza.gh.d@gmail.com>
2022-02-01 13:48:43 +0330

    first

---
+++ js/app.js
@@ -0,0 +1,104 @@
+var map;
+var lat = 36.2935247
+var lng = 59.5579335
+var flag = false
+
+function get_distance(start, end) {
+    x1 = start.lat()
+    y1 = start.lng()
+
+    x2 = end.lat()
+    y2 = end.lng()
+    var distance = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+
+    return distance;
+}
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(lat, lng),
+        zoom: 14,
+    };
+    map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        lat = position.coords.latitude;
+        lng = position.coords.longitude;
+        var myCenter = new google.maps.LatLng(lat, lng);
+
+        map.setCenter(myCenter);
+        var marker_start = new google.maps.Marker({
+            position: myCenter,
+            draggable: true,
+            animation: google.maps.Animation.BOUNCE,
+            icon: 'img/minion.png',
+
+
+
+
+        });
+        marker_start.setMap(map);
+
+        google.maps.event.addListener(marker_start, 'click', (function () {
+
+            // var tag_p_lat = document.getElementById("lat_start");
+            // var tag_p_lng = document.getElementById("lng_start");
+
+            var selected_location = marker_start.getPosition();
+
+            // tag_p_lat.innerHTML = selected_location.lat();
+            // tag_p_lng.innerHTML = selected_location.lng();
+
+            if (flag == false) {
+
+                flag = true
+                var message1 = document.getElementById("message");
+                message1.innerHTML = "مقصد را انتخاب کنید"
+                message1.classList.remove("alert-warning");
+                message1.classList.add("alert-primary");
+                var marker_end = new google.maps.Marker({
+                    position: myCenter,
+                    draggable: true,
+                    animation: google.maps.Animation.BOUNCE,
+
+
+
+
+                });
+                marker_end.setMap(map);
+
+                google.maps.event.addListener(marker_end, 'click', (function () {
+
+                    // var tag_p_lat = document.getElementById("lat_end");
+                    // var tag_p_lng = document.getElementById("lng_end");
+
+                    var selected_location_end = marker_end.getPosition();
+
+                    // tag_p_lat.innerHTML = selected_location_end.lat();
+                    // tag_p_lng.innerHTML = selected_location_end.lng();
+
+                    distance = get_distance(selected_location, selected_location_end)
+                    var price = Math.round(distance * 1000);
+
+                    price = price.toFixed(2)
+
+                    var message = document.getElementById("message");
+                    message.innerHTML = "در خواست سفر شما ثبت شد هزینه سفر " + price + " هزار تومان";
+                    message.classList.remove("alert-warning");
+                    message.classList.add("alert-success");
+                    showNotification();
+                }))
+
+            }
+        }))
+
+    });
+}
+else {
+    alert("Geolocation is not supported by this browser.");
+}
+
+
---
+++ js/notification.js
@@ -0,0 +1,38 @@
+function notify(){
+  var notify=new Notification("new trip" ,{
+      body:"the passengere is waiting",
+      icon:"img/taxi.png",
+      
+
+  } );
+
+  setTimeout(function(){notify.close()},5000);
+
+}
+
+
+function showNotification(){
+    if("Notification" in window){
+        if(Notification.permission==="granted"){
+          notify();
+        }
+        else if(Notification.permission==="denied"){
+         Notification.requestPermission(function(permission){
+           if (permission === "granted" ){
+             notify()
+           }
+         })
+        }
+
+    }
+    else{
+        alert("Notification does not supported");
+    }
+
+
+
+
+
+
+
+}
